ce3549eed6ffcf726847f2230df316af53a3cba8
Mary Mbaoma <mmaryraphaella@gmail.com>
2023-07-31 21:51:05 +0000

    complete script that uploades state files to s3

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import boto3
 import logging
 from dotenv import load_dotenv
 from json_log_formatter import JsonFormatter
+import json
+from datetime import datetime
+import zipfile
 
 def setup_logger():
     # Create a custom logger
@@ -59,34 +62,92 @@ def get_workspaces(ORGANIZATION, TOKEN):
     
     return workspaces_data
 
-def backup_workspaces(workspaces, TOKEN):
+def get_download_url(workspaces, TOKEN):
     headers = {
         'Authorization': f'Bearer {TOKEN}',
         'Content-Type': 'application/vnd.api+json'
     }
 
-    s3 = boto3.client('s3')
-
     for workspace in workspaces:
         workspace_id = workspace['id']
-        logger.info(f'Initiating backup for workspace with ID: {workspace_id}...')
 
         url = f'https://app.terraform.io/api/v2/workspaces/{workspace_id}/current-state-version'
         response = requests.get(url, headers=headers)
 
         if response.status_code != 200:
             logger.error(f'Failed to get current state version for workspace {workspace_id}.')
-        else:
-            logger.info(f'Got the current state version for workspace {workspace_id}')
             continue
 
-def main():
-    global logger
-    logger = setup_logger()
-    ORGANIZATION, S3_BUCKET, TOKEN = get_env_vars()
-    workspaces = get_workspaces(ORGANIZATION, TOKEN)
-    backup_workspaces(workspaces, TOKEN)
+        state_download_url = response.json()["data"]["attributes"]["hosted-state-download-url"]
+        logger.info(f'Got the current state version for workspace {workspace_id}. Proceeding with backup...')
+        # logger.info(f'state download url {state_download_url}')
+        return state_download_url 
 
+def save_state_to_local_file(workspaces, state_download_url):
+    for workspace in workspaces:
+        workspace_id = workspace['id']
+
+        filename = f'{workspace_id}.tfstate'
+        state_response = requests.get(state_download_url)
+
+        # Check if the request was successful
+        if state_response.status_code != 200:
+            logger.error(f'State file not found for workspace {workspace_id}.')
+            continue  # Skip the current iteration and move to the next workspace
+
+        with open(filename, 'w') as f:
+            json.dump(state_response.json(), f)
+        
+        logger.info(f'Saved state file for workspace {workspace_id} as {filename}.')
+        return filename
+
+def zip_state_file(filename, workspaces):
+    for workspace in workspaces:
+        workspace_id = workspace['id']
+
+        # Check if state file exists
+        if not os.path.exists(filename):
+            logger.error(f'State file {filename} not found for workspace {workspace_id}. No file to zip.')
+            continue 
+
+        zipname = f'{datetime.now().strftime("%s")}_{workspace_id}-backup.zip'
+        with zipfile.ZipFile(zipname, 'w') as zipf:
+            zipf.write(filename)
+        logger.info(f'zip name {zipname}')
+        return zipname
+
+def format_s3_key(workspaces, ORGANIZATION):   
+    for workspace in workspaces:
+        workspace_id = workspace['id']
+
+    timestamp = datetime.now().strftime("%s")
+    s3_key = f'{datetime.now().strftime("%Y-%m-%d")}/terraform-{ORGANIZATION}/{timestamp}_{workspace_id}-backup.zip'
+    logger.info(f's3-key {s3_key}')
+    return s3_key
+
+def upload_state_to_s3(S3_BUCKET, zipname, s3_key, filename):
+    s3 = boto3.client('s3')
+    s3.upload_file(zipname, S3_BUCKET, s3_key)
+   
+    os.remove(filename)
+    os.remove(zipname)
+
+def main():
+    try:
+        logging.info('Backup process started.')
+        global logger
+        logger = setup_logger()
+        ORGANIZATION, S3_BUCKET, TOKEN = get_env_vars()
+        workspaces = get_workspaces(ORGANIZATION, TOKEN)
+        state_download_url = get_download_url(workspaces, TOKEN)
+        filename = save_state_to_local_file(workspaces, state_download_url) 
+        zipname = zip_state_file(filename, workspaces)
+        s3_key = format_s3_key(workspaces, ORGANIZATION)
+        upload_state_to_s3(S3_BUCKET, zipname, s3_key, filename)
+    finally:
+        exit(0)
+        
 if __name__ == "__main__":
     load_dotenv()
+    
     main()
