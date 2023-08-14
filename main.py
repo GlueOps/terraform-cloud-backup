f0252bb0ea0be6689729f4554d9aedfc5c6366b4
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-13 17:05:06 -0700

    feat: specifying path of `terraform_cloud` to write files too

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def save_state_to_remote_file(s3_key, workspace, state_download_url, S3_BUCKET):
 
         # Write the ZIP archive to S3
         zip_buffer.seek(0) # Rewind the buffer to the beginning
-        with smart_open.open(f's3://{S3_BUCKET}/{s3_key}', 'wb') as f:
+        with smart_open.open(f's3://{S3_BUCKET}/terraform_cloud/{s3_key}', 'wb') as f:
             f.write(zip_buffer.read())
 
         logger.info(f'Saved state file for workspace {workspace_id} to {S3_BUCKET} with key {s3_key}.')
