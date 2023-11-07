1f3c3a20b3d90804a87410261894a0eb6621f438
Venkata Mutyala <venkata@venkatamutyala.com>
2023-11-06 19:39:46 -0800

    fix: exit code

--- main.py
+++ main.py
@@ -144,9 +144,9 @@ def main():
                 save_state_to_remote_file(s3_key, workspace, state_download_url, env_vars['S3_BUCKET'])
     except Exception as e:  # Catch any exceptions
         logger.exception('An error occurred: %s', e)  # Log the exception
-    finally:
-        exit(0)
-
+        exit(1)
+        
+    exit(0)
 if __name__ == "__main__":
     load_dotenv()
     main()
